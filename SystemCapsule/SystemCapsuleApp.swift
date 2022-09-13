0f5febbb39f2dc4c93f73df567708e67b140289c
梁程 <liangcheng@holyblade.com>
2022-09-13 23:20:40 +0800

    first commit

---
+++ SystemCapsule/SystemCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemCapsuleApp.swift
+//  SystemCapsule
+//
+//  Created by Cheng Liang(Louis) on 2022/9/13.
+//
+
+import SwiftUI
+
+@main
+struct SystemCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
